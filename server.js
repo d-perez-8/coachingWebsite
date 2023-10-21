1b211c3d4b6a2157d8e1123512db7839b3b197c8
Danny Perez <xdanny28x@gmail.com>
2023-10-21 14:02:36 -0700

    add prettier and pictures for navbar

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-require("dotenv").config()
+require("dotenv").config();
 
 // This is your test secret API key.
-const stripe = require('stripe')('sk_test_51NZLHbJDAvhaBoGUIinFDic2oHrOsl4zK7h1gMxU0qx4qJv84VE60TLJrr2MUl2M0FBLyYzxCgntTU3Lps3bzYcP00lB6ViHB6');
-const express = require('express');
+const stripe = require("stripe")(
+  "pk_test_51NZLHbJDAvhaBoGUEm2FIyGv5MyBq9S1mwHNVmc3P1nDv7qZtRkyp2Up7zpfMeIoYoLF1JArM4jc9PCYDVQxFBYd00WMdZXjAT"
+);
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const YOUR_DOMAIN = 'http://localhost:3000';
+const YOUR_DOMAIN = "http://localhost:3000";
 
-app.post('/create-checkout-session', async (req, res) => {
+app.post("/create-checkout-session", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1O3NcVJDAvhaBoGUha5W9Ezr',
+        price: "price_1O3NcVJDAvhaBoGUha5W9Ezr",
         quantity: 1,
       },
     ],
-    mode: 'payment',
+    mode: "payment",
     success_url: `${YOUR_DOMAIN}/success.html`,
     cancel_url: `${YOUR_DOMAIN}/cancel.html`,
   });
@@ -25,4 +27,4 @@ app.post('/create-checkout-session', async (req, res) => {
   res.redirect(303, session.url);
 });
 
-app.listen(3000, () => console.log('Running on port 3000'));
+app.listen(3000, () => console.log("Running on port 3000"));
