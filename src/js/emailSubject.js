372d27acdc9ebe610eeb71ad4eaa13e154e0013d
Danny Perez <xdanny28x@gmail.com>
2023-11-21 10:43:22 -0800

    add dynamic subject field for notification email

---
+++ src/js/emailSubject.js
@@ -0,0 +1,5 @@
+function updateSubject(username) {
+    document.getElementById("output-email-subject").value = `${username} has emailed you from whatcomesaftercoaching.com`;
+    }
+    
+    $(updateSubject());
