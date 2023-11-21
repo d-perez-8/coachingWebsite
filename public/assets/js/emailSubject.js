f83b042ad7cd07e83f2d015fb11a19f7eb0496bb
Danny Perez <xdanny28x@gmail.com>
2023-11-21 11:16:53 -0800

    add dynamic subject field for notification email

--- public/assets/js/emailSubject.js
+++ public/assets/js/emailSubject.js
@@ -1,4 +1,3 @@
-function updateSubject(username) {
-    console.log(`Hi ${username}!`);
-    return document.getElementById("output-email-subject").value = `${username} has emailed you from whatcomesaftercoaching.com`;
+function updateSubject(subject) {
+    return document.getElementById("output-email-subject").value = subject
 }
